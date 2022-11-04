b45bc1a8ea1c8b21dd8dc7117e76fdac0b257327
Kushagra Chaudhary <114591698+Klaus0110@users.noreply.github.com>
2022-11-04 21:32:24 +0530

    Add files via upload

---
+++ Day 15/Pascal.py
@@ -0,0 +1,15 @@
+from math import factorial
+
+n = int(input("Enter the number of rows: "))
+if(n<=0):
+    print("Enter a positive value!")
+
+else:
+    for i in range(n):
+        for j in range(n - i + 1):
+            print(end=" ")
+
+        for j in range(i + 1):
+            print(factorial(i) // (factorial(j) * factorial(i - j)), end=" ")
+
+        print()

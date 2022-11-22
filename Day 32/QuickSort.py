7e55adb7dfdb31c8e848706134cc49317d789c35
Kushagra Chaudhary <114591698+Klaus0110@users.noreply.github.com>
2022-11-22 20:54:17 +0530

    Add files via upload

---
+++ Day 32/QuickSort.py
@@ -0,0 +1,26 @@
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+
+def quick_sort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
+
+
+if __name__ == '__main__':
+    n = int(input("Enter the number of elements in the array: "))
+    array = []
+    print("Enter the elements: ")
+    for i in range(0, n):
+        array.append(int(input()))
+    quick_sort(array, 0, len(array)-1)
+    print(f'Sorted array: {array}')
